be45e298bc7c01045d67e24651027761ad6c196e
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2023-04-27 18:17:22 +0530

    Create easy_Start_prgm2.c

---
+++ easy_Start_prgm2.c
@@ -0,0 +1,22 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int i,j,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(j=n;j>=i;j--){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
+//out put
+*****
+****
+***
+**
+*
