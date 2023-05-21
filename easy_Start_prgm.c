50f9679cb3bb180b107f4f7ab173bbd68af99cb9
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2023-05-21 19:11:09 +0530

    Update easy_Start_prgm.c

--- easy_Start_prgm.c
+++ easy_Start_prgm.c
@@ -1,22 +1,22 @@
-// Online C compiler to run C program online
-#include <stdio.h>
-
-int main() {
-    int i,j,n;
-    scanf("%d",&n);
-    for(i=1;i<=n;i++){
-        for(j=n;j>=i;j--){
-            printf("*");
-        }
-        printf("\n");
-    }
-
-    return 0;
-}
-
-//out put 
+/*
 *
 **
 ***
 ****
 *****
+*/
+
+#include<stdio.h>
+
+int main(){
+    int i,j,n;
+    
+    printf("enter the number of rows : ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
